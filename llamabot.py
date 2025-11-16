f4854a45eb71e3ee0cdbe1f258522309f83a7dbf
Joseph Greef <joseph.k.greef@gmail.com>
2025-11-15 23:48:26 -0800

    Add most basic Discord bot

---
+++ llamabot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import asyncio
+import os
+
+import discord
+from discord.ext import commands
+import dotenv
+
+dotenv.load_dotenv()
+discord_token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or('!'),
+    intents=intents,
+)
+
+@bot.event
+async def on_ready():
+    assert bot.user is not None
+
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+async def main():
+    async with bot:
+        await bot.start(discord_token)
+
+asyncio.run(main())
