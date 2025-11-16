5117432ad6ff8d1aeaa7cae3dd68653e436e7484
Joseph Greef <joseph.k.greef@gmail.com>
2025-11-16 01:19:29 -0800

    Add a youtube downloader for getting sounds

--- sound_management_cog.py
+++ sound_management_cog.py
@@ -7,10 +7,13 @@ import traceback
 import discord
 from discord.ext import commands
 import pydub
+import yt_dlp
+
 
 class SoundManagementCog(commands.Cog):
     def __init__(self, bot):
         self.__bot = bot
+
 
 
     @commands.command()
@@ -35,12 +38,13 @@ class SoundManagementCog(commands.Cog):
             await ctx.reply('Must attach audio file')
             return
 
-        extension = ''.join(pathlib.Path(attachment.filename).suffixes)
 
         file_path = pathlib.Path('./sounds/{}/{}/{}.mp3'.format(ctx.author.name,
                                                                 guild.name,
                                                                 sound_name,
                                                                ))
+
+        extension = ''.join(pathlib.Path(attachment.filename).suffixes)
         work_path = pathlib.Path('./work/attachment{}'.format(extension))
 
         work_path.parent.mkdir(parents=True, exist_ok=True)
@@ -57,9 +61,64 @@ class SoundManagementCog(commands.Cog):
 
 
     @commands.command()
-    async def add_sound_youtube(self, ctx, *args):
-        #TODO
-        pass
+    async def add_sound_youtube(self,
+                                ctx,
+                                url,
+                                start_time,
+                                end_time,
+                                sound_name,
+                                sound_weight,
+                                guild_identifier=None,
+                               ):
+        try:
+            guild = self.__parse_guild(ctx, guild_identifier) 
+            (sound_name, sound_weight) = self.__parse_sound_info(sound_name,
+                                                                 sound_weight) 
+            start_time = float(start_time)
+            end_time = float(end_time)
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+            await ctx.reply(str(e))
+
+        async with ctx.typing():
+            try:
+                yt_opts = {
+                    'verbose': True,
+                    'download_ranges': 
+                        yt_dlp.utils.download_range_func(None, [(start_time,
+                                                                 end_time)]),
+                    'force_keyframes_at_cuts': True,
+                    'format':'ba.2', #2nd best audio only format
+                    'outtmpl':'work/youtube.%(ext)s',
+                    'noplaylist':True,
+                }
+
+
+                with yt_dlp.YoutubeDL(yt_opts) as ydl:
+                    info_dict = ydl.extract_info(url, download=True)
+                    work_path = pathlib.Path(ydl.prepare_filename(info_dict))
+
+
+                file_path = pathlib.Path('./sounds/{}/{}/{}.mp3'.format(ctx.author.name,
+                                                                        guild.name,
+                                                                        sound_name,
+                                                                       ))
+                    
+            except Exception as e:
+                traceback.print_exc()
+                print(e)
+
+
+            try:
+                self.__process_sound(work_path, file_path, sound_weight)
+                await ctx.reply('Successfully added sound {} to server {}'.format(
+                                    sound_name, guild.name))
+            except Exception as e:
+                await ctx.reply('Could not convert audio file: ' + str(e))
+                print(e)
+                raise
+
 
 
     @commands.command()
@@ -149,6 +208,9 @@ class SoundManagementCog(commands.Cog):
             except Exception as e:
                 print(e)
             await ctx.reply(file=file, content='Here is {}'.format(sound_name))
+
+    def __add_sound_common(self, arg):
+        pass
 
 
     def __parse_guild(self, ctx, guild_identifier):
